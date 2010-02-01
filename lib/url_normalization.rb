6ba8afc0deade9369b06399790845c2bd7e5e4f4
Scott Taylor <scott@railsnewbie.com>
2010-02-01 15:06:14 -0500

    Call super instead of write_attribute

--- lib/url_normalization.rb
+++ lib/url_normalization.rb
@@ -2,7 +2,7 @@ module UrlNormalization
   def normalize_url(field_name)
     define_method "#{field_name}=" do |val|
       val = "http://#{val}" if !val.blank? && !(val =~ /^http/)
-      write_attribute(field_name, val)
+      super(val)
     end
   end
 end
