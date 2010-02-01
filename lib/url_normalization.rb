4de7d5ec5b2dcfcff349ec5206d003c749a04460
Scott Taylor <scott@railsnewbie.com>
2010-02-01 15:07:50 -0500

    Sttrip whitespace from urls

--- lib/url_normalization.rb
+++ lib/url_normalization.rb
@@ -1,6 +1,7 @@
 module UrlNormalization
   def normalize_url(field_name)
     define_method "#{field_name}=" do |val|
+      val.strip! if val.respond_to?(:strip!)
       val = "http://#{val}" if !val.blank? && !(val =~ /^http/)
       super(val)
     end
--- spec/url_normalization/url_normalization_spec.rb
+++ spec/url_normalization/url_normalization_spec.rb
@@ -9,7 +9,7 @@ describe UrlNormalization do
     before do
       @user = User.new
     end
-    
+
     it "should not add http:// if it was already included" do
       @user.url = "http://example.com"
       @user.url.should == "http://example.com"
@@ -19,7 +19,7 @@ describe UrlNormalization do
       @user.url = "example.com"
       @user.url.should == "http://example.com"
     end
- 
+
     it "should not add http:// if https:// was already included" do
       @user.url = "https://example.com"
       @user.url.should == "https://example.com"
@@ -33,6 +33,16 @@ describe UrlNormalization do
     it "should not assign the http if nil" do
       @user.url = nil
       @user.url.should be_nil
+    end
+
+    it "should strip whitespace for a url which includes http://" do
+      @user.url = "http://foobar.com    "
+      @user.url.should == "http://foobar.com"
+    end
+
+    it "should strip a url which does not include http" do
+      @user.url = "     foobar.com    "
+      @user.url.should == "http://foobar.com"
     end
   end
 end
