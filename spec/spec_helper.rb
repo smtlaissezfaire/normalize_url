5f1c239a3e69cd399dcddb9ddff70c0f3bfb9403
Scott Taylor <scott@railsnewbie.com>
2010-02-01 15:08:14 -0500

    Use require active_record, not require activerecord

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "rubygems"
-require "activerecord"
+require "active_record"
 require File.expand_path(File.dirname(__FILE__) + "/../lib/url_normalization")
 require File.expand_path(File.dirname(__FILE__) + "/../init")
 
